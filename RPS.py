f57c6fa9b0a325589aa0f4c391d8174026159b13
AbdulRehmanKhan101 <ark900500@gmail.com>
2026-06-27 15:21:23 +0500

    Improve player function with move prediction logic
    Enhance player strategy by predicting opponent's moves based on history.
    
    Signed-off-by: AbdulRehmanKhan101 <ark900500@gmail.com>

--- RPS.py
+++ RPS.py
@@ -1,10 +1,23 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play:
+        opponent_history.append(prev_play)
 
     guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+
+    if len(opponent_history) > 5:
+        pattern_len = 3
+        recent = "".join(opponent_history[-pattern_len:])
+
+        move_counts = {"R": 0, "P": 0, "S": 0}
+
+        for i in range(len(opponent_history) - pattern_len):
+            window = "".join(opponent_history[i:i+pattern_len])
+            if window == recent:
+                next_move = opponent_history[i + pattern_len]
+                move_counts[next_move] += 1
+
+        predicted = max(move_counts, key=move_counts.get)
+        counter = {"R": "P", "P": "S", "S": "R"}
+        guess = counter[predicted]
 
     return guess
